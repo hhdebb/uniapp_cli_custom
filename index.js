3170e49c05f1fffa9e2eab8f73939f63c5251d63
hhdebb <git.hhdebb@gmail.com>
2022-03-03 13:52:21 +0800

    ‘req fs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var fs = require('fs');
+const fs = require('fs');
 
-let current_path = process.cwd();
+const current_path = process.cwd();
 
 let mp_file = current_path + "/node_modules/@dcloudio/webpack-uni-pages-loader/lib/platforms/mp.js";
 
@@ -8,19 +8,20 @@ fs.readFile(mp_file, 'utf8', function (err, data)
 {
     if (err)
     {
-        return console.log("读取文件错误:", err);
+       return console.error(" 读取文件错误：\r\n", err);
     }
     
-    var result = data.replace(/json\.projectname = value/g, 'json.projectname = process.env.VUE_APP_PROJECT_NAME');
+    let result = data.replace(/json\.projectname = value/g, 'json.projectname = process.env.VUE_APP_PROJECT_NAME');
     result = result.replace(/project\.appid = 'touristappid'/g, 'project.appid = ""');
     
     fs.writeFile(mp_file, result, 'utf8', function (err)
     {
         if (err)
         {
-            return console.log("写入文件错误:", err);
+            throw "写入文件错误:" + err;
+            //return console.error("写入文件错误:", err);
         }
         
-        return console.log("修改成功：" + mp_file);
+        return console.info("修改成功：" + mp_file);
     });
 });
